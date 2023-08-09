373d596930ed94d6510f16799f1e4c444b371928
Jake Priestman <jake.priestman@evidi.com>
2023-08-09 13:18:33 +0200

    added content

---
+++ js/site.js
@@ -0,0 +1,53 @@
+﻿function ScrollFunction() {
+
+    var header = document.getElementById("header");
+    var section = document.documentElement.section;
+
+    if (2 * document.documentElement.clientHeight <= document.getElementById("container").scrollTop && document.getElementById("container").scrollTop < 3*document.documentElement.clientHeight){
+        if (header.className == "topnav") {
+            document.getElementsByClassName("topnav")[0].style.backgroundColor = '#FBEAD1';
+        }
+        else if (header.className == "topnav responsive") {
+            document.getElementsByClassName("topnav responsive")[0].style.backgroundColor = '#FBEAD1';
+            //document.getElementsById("topnavUL").style.backgroundColor = 'rgb(33, 52, 69)';
+        }
+    }
+    if (3 * document.documentElement.clientHeight <= document.getElementById("container").scrollTop) {
+        if (header.className == "topnav") {
+            document.getElementsByClassName("topnav")[0].style.backgroundColor = '#19d175';
+        }
+        else if (header.className == "topnav responsive") {
+            document.getElementsByClassName("topnav responsive")[0].style.backgroundColor = '#19d175';
+            //document.getElementsById("topnavUL").style.backgroundColor = 'rgb(172, 194, 215)';
+        }
+    }
+    if (2 * document.documentElement.clientHeight > document.getElementById("container").scrollTop) {
+        if (header.className == "topnav") {
+            document.getElementsByClassName("topnav")[0].style.backgroundColor = 'transparent';
+        }
+        else if (header.className == "topnav responsive") {
+            document.getElementsByClassName("topnav responsive")[0].style.backgroundColor = 'rgb(56, 88, 120)';
+            //document.getElementById("topnavUL").style.backgroundColor = 'rgb(56, 88, 120)'
+        }
+    }
+}
+
+function LoaderFunction() {
+    var myVar = setTimeout(showPage, 4000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("container").style.display = "block";
+}
+
+
+
+function HamburgerFunction() {
+    var x = document.getElementById("header");
+    if (x.className == "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
